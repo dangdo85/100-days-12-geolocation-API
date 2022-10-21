41499c0d6cb78a3c0e336bc73070cf995c34b3b8
dangdo85 <danghdo@gmail.com>
2022-10-21 16:35:12 -0400

    added copy buttons feature

---
+++ script.js
@@ -0,0 +1,45 @@
+const userLatitude = document.querySelector(".latitude");
+const userLongitude = document.querySelector(".longitude");
+const btnLoc = document.querySelector(".btn-loc");
+const btnLong = document.querySelector(".btn-copy-long");
+const btnLat = document.querySelector(".btn-copy-lat");
+const latG = '';
+const longG = '';
+
+
+btnLoc.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(showPosition);
+});
+
+function showPosition(position) {
+  userLatitude.innerHTML = position.coords.latitude
+  userLongitude.innerHTML = position.coords.longitude
+}
+
+const copyLat = (e) => {
+  e.preventDefault();
+
+  navigator.clipboard.writeText(userLatitude.innerHTML)
+    .then(() => {
+      btnLat.textContent = "Copied!!!";
+      setTimeout(() => {
+        btnLat.textContent = "Copy";
+      }, 3000);
+    })
+};
+
+const copyLong = (e) => {
+  e.preventDefault();
+
+  navigator.clipboard.writeText(userLongitude.innerHTML)
+    .then(() => {
+      btnLong.textContent = "Copied!!!";
+      setTimeout(() => {
+        btnLong.textContent = "Copy";
+      }, 3000);
+    })
+};
+
+
+btnLat.addEventListener("click", copyLat);
+btnLong.addEventListener("click", copyLong);
