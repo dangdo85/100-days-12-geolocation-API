2d5be80708e60fb3610416d4311a9e3481ee2d4f
dangdo85 <danghdo@gmail.com>
2022-10-21 16:38:29 -0400

    removed unused consts

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const userLongitude = document.querySelector(".longitude");
 const btnLoc = document.querySelector(".btn-loc");
 const btnLong = document.querySelector(".btn-copy-long");
 const btnLat = document.querySelector(".btn-copy-lat");
-const latG = '';
-const longG = '';
-
-
-btnLoc.addEventListener("click", () => {
-  navigator.geolocation.getCurrentPosition(showPosition);
-});
 
 function showPosition(position) {
   userLatitude.innerHTML = position.coords.latitude
@@ -18,28 +11,30 @@ function showPosition(position) {
 
 const copyLat = (e) => {
   e.preventDefault();
-
+  
   navigator.clipboard.writeText(userLatitude.innerHTML)
-    .then(() => {
-      btnLat.textContent = "Copied!!!";
-      setTimeout(() => {
-        btnLat.textContent = "Copy";
-      }, 3000);
-    })
+  .then(() => {
+    btnLat.textContent = "Copied!!!";
+    setTimeout(() => {
+      btnLat.textContent = "Copy";
+    }, 3000);
+  })
 };
 
 const copyLong = (e) => {
   e.preventDefault();
-
+  
   navigator.clipboard.writeText(userLongitude.innerHTML)
-    .then(() => {
-      btnLong.textContent = "Copied!!!";
-      setTimeout(() => {
-        btnLong.textContent = "Copy";
-      }, 3000);
-    })
+  .then(() => {
+    btnLong.textContent = "Copied!!!";
+    setTimeout(() => {
+      btnLong.textContent = "Copy";
+    }, 3000);
+  })
 };
 
-
+btnLoc.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(showPosition);
+});
 btnLat.addEventListener("click", copyLat);
 btnLong.addEventListener("click", copyLong);
